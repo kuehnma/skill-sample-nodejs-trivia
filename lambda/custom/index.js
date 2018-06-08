34a21d9eab3adcf54dd024dc02bae498b8d7134c
Markus <mail@markus-kuehn.com>
2018-06-08 20:54:00 +0200

    einleitungstext geändert

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -231,48 +231,11 @@ function helpTheUser(newGame, handlerInput) {
 
 /* jshint -W101 */
 const languageString = {
-  en: {
-    translation: {
-      QUESTIONS: questions.QUESTIONS_EN_US,
-      GAME_NAME: 'Reindeer Trivia',
-      HELP_MESSAGE: 'I will ask you %s multiple choice questions. Respond with the number of the answer. For example, say one, two, three, or four. To start a new game at any time, say, start game. ',
-      REPEAT_QUESTION_MESSAGE: 'To repeat the last question, say, repeat. ',
-      ASK_MESSAGE_START: 'Would you like to start playing?',
-      HELP_REPROMPT: 'To give an answer to a question, respond with the number of the answer. ',
-      STOP_MESSAGE: 'Would you like to keep playing?',
-      CANCEL_MESSAGE: 'Ok, let\'s play again soon.',
-      NO_MESSAGE: 'Ok, we\'ll play another time. Goodbye!',
-      TRIVIA_UNHANDLED: 'Try saying a number between 1 and %s',
-      HELP_UNHANDLED: 'Say yes to continue, or no to end the game.',
-      START_UNHANDLED: 'Say start to start a new game.',
-      NEW_GAME_MESSAGE: 'Welcome to %s. ',
-      WELCOME_MESSAGE: 'I will ask you %s questions, try to get as many right as you can. Just say the number of the answer. Let\'s begin. ',
-      ANSWER_CORRECT_MESSAGE: 'correct. ',
-      ANSWER_WRONG_MESSAGE: 'wrong. ',
-      CORRECT_ANSWER_MESSAGE: 'The correct answer is %s: %s. ',
-      ANSWER_IS_MESSAGE: 'That answer is ',
-      TELL_QUESTION_MESSAGE: 'Question %s. %s ',
-      GAME_OVER_MESSAGE: 'You got %s out of %s questions correct. Thank you for playing!',
-      SCORE_IS_MESSAGE: 'Your score is %s. '
-    },
-  },
-  'en-US': {
-    translation: {
-      QUESTIONS: questions.QUESTIONS_EN_US,
-      GAME_NAME: 'American Reindeer Trivia'
-    },
-  },
-  'en-GB': {
-    translation: {
-      QUESTIONS: questions.QUESTIONS_EN_GB,
-      GAME_NAME: 'British Reindeer Trivia'
-    },
-  },
   de: {
     translation: {
       QUESTIONS: questions.QUESTIONS_DE_DE,
-      GAME_NAME: 'Wissenswertes über Rentiere in Deutsch',
-      HELP_MESSAGE: 'Ich stelle dir %s Multiple-Choice-Fragen. Antworte mit der Zahl, die zur richtigen Antwort gehört. Sage beispielsweise eins, zwei, drei oder vier. Du kannst jederzeit ein neues Spiel beginnen, sage einfach „Spiel starten“. ',
+      GAME_NAME: 'Glücks Tagebuch',
+      HELP_MESSAGE: 'Ich stelle dir 3 Fragen, die dir helfen soll dich an positive Ereignisse zu erinnern.',
       REPEAT_QUESTION_MESSAGE: 'Wenn die letzte Frage wiederholt werden soll, sage „Wiederholen“ ',
       ASK_MESSAGE_START: 'Möchten Sie beginnen?',
       HELP_REPROMPT: 'Wenn du eine Frage beantworten willst, antworte mit der Zahl, die zur richtigen Antwort gehört. ',
